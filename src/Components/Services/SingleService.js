9d9028de5a54f656379846107f2cefaf276bb9b6
Tazwar <tazwar9t6@gmail.com>
2020-11-20 12:38:09 +0600

    added hover on services

--- src/Components/Services/SingleService.js
+++ src/Components/Services/SingleService.js
@@ -8,7 +8,7 @@ const SingleService = ({sData}) => {
         <div className="mt-5">           
                 <div className="d-flex justify-content-between ">
                 <div className="col-md-3  p-5">
-                <Card className="text-center service-card-container p-5" style={{ width: '18rem' }}>
+                <Card className="text-center service-card-container border-0 p-5" style={{ width: '18rem' }}>
   <Card.Img className="w-25 mx-auto d-block" variant="top" src={image} />
   <Card.Body >
     <Card.Title>{title}</Card.Title>
